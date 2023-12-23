9a4e634209e0d5ec62d89f18ee69f4b9f557d18e
Puyu <49837577+Z-Puyu@users.noreply.github.com>
2023-12-23 16:20:15 +0800

    Revamp and generalise construction queue

--- static/BuildingManager.gd
+++ static/BuildingManager.gd
@@ -6,45 +6,43 @@ enum BUILING_TYPES {
 
 var buildings: Dictionary # [Vector2i, Node]
 var available_buildings: Dictionary # [Integer, PackedScene]
-var curr_spawning_obj: Node2D
-var queues: Array[LinkedList]
-var max_capacity: int
-var under_construction: Dictionary
+var curr_spawning_obj: int
+# var queues: Array[LinkedList]
+# var max_capacity: int
+# var under_construction: Dictionary
+var construction_queue: ConstructionQueue
 
-signal spawn_building(building: Node2D)
+signal spawn_building(building: Node2D, pos: Vector2i)
 
 func _ready():
-	curr_spawning_obj = null
 	buildings = {}
 	available_buildings[0] = load("res://assets/buildings/ForestryBuilding.tscn")
-	queues = []   
-	max_capacity = 5
-	for i in max_capacity:
-		queues.append(LinkedList.new())
-	GameManager.game_clock.timeout.connect(next_day)
+	construction_queue = ConstructionQueue.new(2)
+	spawn_building.connect(on_building_complete)
 
 func add_building(pos: Vector2i, coords: Vector2, type: int) -> bool:
 	# Note that pos is the local map coordinates
-	if curr_spawning_obj == null:
-		curr_spawning_obj = available_buildings[type].instantiate()
-	if curr_spawning_obj.can_be_built():
+	curr_spawning_obj = type
+	var obj: Node2D = available_buildings.get(curr_spawning_obj).instantiate()
+	if obj.can_be_built():
 		if buildings.has(pos):
 			print(str("Cell ", pos, " already has a building!"))
-			var building: Node2D = buildings.get(pos)
-			if under_construction.has(building):
+			var building: Node = buildings.get(pos)
+			if building is ConstructibleTask:
 				# If the building is under construction, we return the cost
 				# User Validation panel needed here
-				ResourceManager.add(building.data.cost)
-				under_construction.erase(building)
+				ResourceManager.add(building.value().data.cost)
+				buildings.erase(building)
 			else:
 				# User Validation panel needed here
 				delete_building(pos) 
-		var obj: Node2D = curr_spawning_obj.duplicate()
 		ResourceManager.consume(obj.data.cost)
-		buildings[pos] = obj
 		obj.translate(coords)
-		under_construction[obj] = obj.data.time_to_build
-		queues[under_construction.size() % max_capacity].push_back(obj)
+		var task: ConstructibleTask = ConstructibleTask.new(obj, pos)
+		buildings[pos] = task
+		construction_queue.enqueue(task)
+		# under_construction[obj] = obj.data.time_to_build
+		# queues[under_construction.size() % max_capacity].push_back(obj)
 		return true
 	return false
 		
@@ -56,16 +54,6 @@ func delete_building(pos: Vector2i):
 		building.queue_free()
 	else:
 		print(str("Cell ", pos, " has no building!"))
-		
-func next_day():
-	for q in queues:
-		if not q.is_empty():
-			var new_building: Node2D = q.peek_front()
-			while not under_construction.has(new_building):
-				q.pop_front()
-				new_building = q.peek_front()
-			under_construction[new_building] -= 1
-			if under_construction[new_building] == 0:
-				q.pop_front()
-				under_construction.erase(new_building)
-				spawn_building.emit(new_building)
+				
+func on_building_complete(building: Node2D, pos: Vector2i):
+	buildings[pos] = building
