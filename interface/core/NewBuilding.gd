105cace605b64625bd459ecb5f43c8de98231cc9
Luo Zhiyang <e0969568@u.nus.edu>
2023-12-27 23:32:28 +0800

    menu wip

---
+++ interface/core/NewBuilding.gd
@@ -0,0 +1,12 @@
+extends Button
+
+@onready var building_menu = $BuildingMenu
+
+func _ready():
+	building_menu.hide()
+
+func _on_pressed():
+	if (building_menu.visible):
+		building_menu.hide()
+	else:
+		building_menu.show()
